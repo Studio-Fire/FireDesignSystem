a6a1a7f670d576e16c549206921dd156b6eb6aaf
Seop (SeongHyuk Seo) <seops4850@gmail.com>
2024-07-04 09:58:46 +0900

    Develop FireTheme Template #3 (#4)

---
+++ designsystem/src/main/java/com/fire/designsystem/foundation/FireTheme.kt
@@ -0,0 +1,44 @@
+package com.fire.designsystem.foundation
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.fire.designsystem.foundation.color.FireColors
+import com.fire.designsystem.foundation.color.fireDarkColors
+import com.fire.designsystem.foundation.color.fireLightColors
+import com.fire.designsystem.foundation.dimen.FireDimen
+import com.fire.designsystem.foundation.dimen.fireDimen
+import com.fire.designsystem.foundation.typo.FireTypo
+import com.fire.designsystem.foundation.typo.fireTypo
+
+val LocalFireColors = staticCompositionLocalOf { fireLightColors }
+val LocalFireTypo = staticCompositionLocalOf { fireTypo }
+val LocalFireDimen = staticCompositionLocalOf { fireDimen }
+
+@Composable
+fun FireTheme(
+    colors: FireColors = if (isSystemInDarkTheme()) fireDarkColors else fireLightColors,
+    typo: FireTypo = fireTypo,
+    dimen: FireDimen = fireDimen,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalFireColors provides colors,
+        LocalFireTypo provides typo,
+        LocalFireDimen provides dimen,
+        content = content
+    )
+}
+
+object FireTheme {
+    val colors: FireColors
+        @Composable
+        get() = LocalFireColors.current
+    val typo: FireTypo
+        @Composable
+        get() = LocalFireTypo.current
+    val dimen: FireDimen
+        @Composable
+        get() = LocalFireDimen.current
+}
